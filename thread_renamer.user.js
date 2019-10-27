0348869e1dcfe53b37f8e4a6fce7710cb0b6b5da
Alex P <alex.pantec@gmail.com>
2019-10-27 18:13:00 +0200

    add missing ;

--- thread_renamer.user.js
+++ thread_renamer.user.js
@@ -31,7 +31,7 @@
 
   String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
-  }
+  };
 
   // ================================================================
 
