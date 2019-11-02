855216504fa22dd0b7db73d0334519b1ee2f14a3
Vadim-Moshev <39858114+Vadim-Moshev@users.noreply.github.com>
2019-11-02 16:30:36 +0500

    Add function renumberPosts

--- consts.js
+++ consts.js
@@ -1,6 +1,21 @@
 class Moshev_PFConsts {
-	static get PATH_TO_LOADER_ICON() {
+  static get PATH_TO_LOADER_ICON() {
 		return '/images/misc/progress.gif'
-	};
+	}
 
+  // ==========================================================================
+
+  static renumberPosts(aStart = 1) {
+    if (typeof aStart !== 'number') {
+      throw new TypeError('Аргумент функции renumberPosts должен быть числом.')
+    }
+
+    const hyperReferencesWithPostNumbers = document.querySelectorAll('a[id^="postcount"]');
+    for (let i = 0, newNumber = aStart; i < hyperReferencesWithPostNumbers.length; i++, newNumber++) {
+      const currentHyperReference = hyperReferencesWithPostNumbers[i];
+      currentHyperReference.setAttribute('name', newNumber);
+      currentHyperReference.href = currentHyperReference.href.replace(/postcount=\d+/, `postcount=${newNumber}`)
+      currentHyperReference.querySelector('strong').textContent = newNumber;
+    }
+  }
 }
