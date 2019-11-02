39d1c32654cdd8da47ed1c0330ba573d97ec639d
Vadim-Moshev <39858114+Vadim-Moshev@users.noreply.github.com>
2019-11-02 20:04:42 +0500

    Update consts.js

--- consts.js
+++ consts.js
@@ -1,3 +1,21 @@
+class Programmersforum {
+  static renumberPosts(aStart = 1) {
+    if (typeof aStart !== 'number') {
+      throw new TypeError('Аргумент функции renumberPosts должен быть числом.')
+    }
+
+    const hyperReferencesWithPostNumbers = document.querySelectorAll('a[id^="postcount"]');
+    for (let i = 0, newNumber = aStart; i < hyperReferencesWithPostNumbers.length; i++, newNumber++) {
+      const currentHyperReference = hyperReferencesWithPostNumbers[i];
+      currentHyperReference.setAttribute('name', newNumber);
+      currentHyperReference.href = currentHyperReference.href.replace(/postcount=\d+/, `postcount=${newNumber}`)
+      currentHyperReference.querySelector('strong').textContent = newNumber;
+    }
+  }
+
+  static vv = 42;
+};
+
 class Moshev_PFConsts {
   static get PATH_TO_LOADER_ICON() {
     return '/images/misc/progress.gif'
@@ -18,21 +36,5 @@ class Moshev_PFConsts {
       currentHyperReference.querySelector('strong').textContent = newNumber;
     }
   }
-}
+};
 
-class Programmersforum {
-  static renumberPosts(aStart = 1) {
-    if (typeof aStart !== 'number') {
-      throw new TypeError('Аргумент функции renumberPosts должен быть числом.')
-    }
-
-    const hyperReferencesWithPostNumbers = document.querySelectorAll('a[id^="postcount"]');
-    for (let i = 0, newNumber = aStart; i < hyperReferencesWithPostNumbers.length; i++, newNumber++) {
-      const currentHyperReference = hyperReferencesWithPostNumbers[i];
-      currentHyperReference.setAttribute('name', newNumber);
-      currentHyperReference.href = currentHyperReference.href.replace(/postcount=\d+/, `postcount=${newNumber}`)
-      currentHyperReference.querySelector('strong').textContent = newNumber;
-    }
-  }
-
-  static vv = 42;
