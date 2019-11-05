f5d0c611998d1abc31602ed25fc4be32a5a69658
Vadim-Moshev <39858114+Vadim-Moshev@users.noreply.github.com>
2019-11-06 03:32:02 +0500

    changed renumberPosts function temporary
    This changes are need to correct posts renumbering

--- consts.js
+++ consts.js
@@ -5,17 +5,30 @@ class Moshev_PFConsts {
 
   // ==========================================================================
 
-  static renumberPosts(aStart = 1) {
+  static renumberPosts(aStart = 1, aDeletedPostNumber) {
+    console.log('библиотека подключена')
+
     if (typeof aStart !== 'number') {
       throw new TypeError('Аргумент функции renumberPosts должен быть числом.')
     }
 
+    // пока костыльное быдлорешение. Оно состоит в том, что мы игнорируем удаляемый пост
+
     const hyperReferencesWithPostNumbers = document.querySelectorAll('a[id^="postcount"]');
-    for (let i = 0, newNumber = aStart; i < hyperReferencesWithPostNumbers.length; i++, newNumber++) {
+    for (let i = 0, newNumber = aStart; i < hyperReferencesWithPostNumbers.length; i++) {
       const currentHyperReference = hyperReferencesWithPostNumbers[i];
+
+      if (+currentHyperReference.getAttribute('name') === aDeletedPostNumber) {
+        continue
+      }
+
       currentHyperReference.setAttribute('name', newNumber);
       currentHyperReference.href = currentHyperReference.href.replace(/postcount=\d+/, `postcount=${newNumber}`);
-      currentHyperReference.querySelector('strong').textContent = newNumber
+      currentHyperReference.querySelector('strong').textContent = newNumber;
+
+      newNumber++;
     }
   }
+
+  // ==========================================================================
 }
