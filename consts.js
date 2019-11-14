5cd0dae5f11af4172e933af1e8c8d6da39eccb11
Vadim-Moshev <39858114+Vadim-Moshev@users.noreply.github.com>
2019-11-15 03:56:56 +0500

    fallback renumberPosts function to normal

--- consts.js
+++ consts.js
@@ -5,22 +5,16 @@ class Moshev_PFConsts {
 
   // ==========================================================================
 
-  static renumberPosts(aStart = 1, aDeletedPostNumber) {
-    console.log('библиотека подключена')
+  static renumberPosts(aStart = 1) {
+    console.log('новая библиотека подключена')
 
     if (typeof aStart !== 'number') {
       throw new TypeError('Аргумент функции renumberPosts должен быть числом.')
     }
 
-    // пока костыльное быдлорешение. Оно состоит в том, что мы игнорируем удаляемый пост
-
     const hyperReferencesWithPostNumbers = document.querySelectorAll('a[id^="postcount"]');
     for (let i = 0, newNumber = aStart; i < hyperReferencesWithPostNumbers.length; i++) {
       const currentHyperReference = hyperReferencesWithPostNumbers[i];
-
-      if (+currentHyperReference.getAttribute('name') === aDeletedPostNumber) {
-        continue
-      }
 
       currentHyperReference.setAttribute('name', newNumber);
       currentHyperReference.href = currentHyperReference.href.replace(/postcount=\d+/, `postcount=${newNumber}`);
