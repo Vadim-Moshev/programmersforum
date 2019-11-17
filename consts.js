4f305eca252bfebbb54faf6571122ddc22f77887
Vadim-Moshev <39858114+Vadim-Moshev@users.noreply.github.com>
2019-11-18 00:49:58 +0500

    Add the setSuspendedStatusToUsername() function

--- consts.js
+++ consts.js
@@ -23,4 +23,52 @@ class Moshev_PFConsts {
   }
 
   // ==========================================================================
+
+  static setSuspendedStatusToUsername(aUserName) {
+    const
+      STATUS                          = 'Заблокирован',
+      SMALLFONT_CLASS_SEARCH_SELECTOR = 'table[id^="post"] tr:nth-child(2) td:first-child div.smallfont',
+      SINGATURE_BLOCK_SEARCH_SELECTOR = 'td[id^="td_post_"] div.signature';
+
+
+    if (typeof aUserName !== 'string') {
+      throw new TypeError('Аргумент функции setSuspendedStatusByUsername должен быть строкой.')
+    }
+
+    if (posts === null) {
+      throw new TypeError('Контейнер с сообщениями не обнаружен на текущей странице.')
+    }
+
+    let userPosts = posts.querySelectorAll('div[align="center"]');
+    let currentUserSmallfontElements;
+
+    userPosts = Array.prototype.slice.call(userPosts);
+    userPosts
+      .filter(aCurrentPost => {
+        let userNameContainer = aCurrentPost.querySelector('.bigusername');
+        return userNameContainer !== null && userNameContainer.textContent === aUserName
+      }).forEach(aCurrentPost => {
+        currentUserSmallfontElements = aCurrentPost.querySelectorAll(SMALLFONT_CLASS_SEARCH_SELECTOR);
+        if (currentUserSmallfontElements.length === 1) {
+          let theOnlySmallfont = currentUserSmallfontElements[0];
+
+          let suspendedCaption = document.createElement('div');
+          suspendedCaption.classList.add('smallfont');
+          suspendedCaption.textContent = STATUS;
+
+          theOnlySmallfont.parentNode.insertBefore(suspendedCaption, theOnlySmallfont);
+        } else {
+          currentUserSmallfontElements[0].textContent = STATUS;
+          for (let i = currentUserSmallfontElements.length - 2; i > 0 ; i--) {
+            currentUserSmallfontElements[i].remove()
+          }
+        }
+
+        // удалить подпись
+        let signature = aCurrentPost.querySelector(SINGATURE_BLOCK_SEARCH_SELECTOR);
+        if (signature !== null) {
+          signature.remove()
+        }
+      });
+  }
 }
